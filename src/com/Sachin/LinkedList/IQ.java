b96da73f045d106f6751e45263fb521c7e89c4b9
Sachin <sachinkrj077@gmail.com>
2022-08-31 13:12:40 +0500

    find lenth 0f cycle

--- src/com/Sachin/LinkedList/IQ.java
+++ src/com/Sachin/LinkedList/IQ.java
@@ -35,6 +35,68 @@ class IQ {
         return false;
     }
 
+//  find length of the cycle
+    public int lengthCycle() {
+        if (head == null) {
+            return 0;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+//                calculate the length
+                Node temp = slow;
+                int length = 0;
+                do {
+                    temp = temp.next;
+                    length++;
+                } while (temp != slow) ;
+                return length;
+            }
+        }
+        return 0;
+    }
+
+//  detect from where the cycling is starting
+    public Node detectCycle(Node head) {
+        int length = 0;
+        if (head == null) {
+            return null;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                length = lengthCycle();
+                break;
+            }
+        }
+        // find the start node
+        Node f = head;
+        Node s = head;
+
+        while (length > 0) {
+            s = s.next;
+            length--;
+        }
+        // keep moving both forward and they will meet at cycle start
+        while (f != s) {
+            f = f.next;
+            s = s.next;
+        }
+        return s;
+    }
+
+
+
 
     public static void main(String[] args) {
         IQ list = new IQ();
@@ -47,7 +109,9 @@ class IQ {
         // Creating a loop for testing
         head.next.next.next.next.next = head.next.next;
 
-        System.out.println(list.hasCycle(head)  );
+        System.out.println(list.hasCycle(head));
 
+
+        System.out.println("length : " + list.detectCycle(head));
     }
 }
