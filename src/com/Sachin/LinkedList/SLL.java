792654c0eaa45751d26670c9d1640e90c098c019
Sachin <sachinkrj077@gmail.com>
2022-09-03 09:01:56 +0500

    added bubble short

--- src/com/Sachin/LinkedList/SLL.java
+++ src/com/Sachin/LinkedList/SLL.java
@@ -207,6 +207,44 @@ public class SLL {
         return ans;
     }
 
+    public void bubbleSort() {
+        bubbleSort(size - 1, 0);
+    }
+
+    private void bubbleSort(int row, int col) {
+        if (row == 0) {
+            return;
+        }
+
+        if (col < row) {
+            Node first = get(col);
+            Node second = get(col + 1);
+
+            if (first.value > second.value) {
+                // swap
+                if (first == head) {
+                    head = second;
+                    first.next = second.next;
+                    second.next = first;
+                } else if (second == tail) {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    tail = first;
+                    first.next = null;
+                    second.next = tail;
+                } else {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    first.next = second.next;
+                    second.next = first;
+                }
+            }
+            bubbleSort(row, col + 1);
+        } else {
+            bubbleSort(row - 1, 0);
+        }
+    }
+
 
     public static void main(String[] args) {
         SLL list = new SLL();
@@ -235,6 +273,10 @@ public class SLL {
 
         SLL ans = SLL.merge(first, second);
         ans.display();
+
+        System.out.println("bubble sort");
+        second.bubbleSort();
+        second.display();
     }
 
 
