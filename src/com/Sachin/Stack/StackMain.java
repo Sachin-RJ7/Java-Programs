4da63588d3835ad571b95e3415fc679f0b066965
Sachin <sachinkrj077@gmail.com>
2022-09-05 07:35:09 +0500

    dynamic stack

--- src/com/Sachin/Stack/StackMain.java
+++ src/com/Sachin/Stack/StackMain.java
@@ -2,13 +2,15 @@ package com.Sachin.Stack;
 
 public class StackMain {
     public static void main(String[] args) throws StackException {
-        CustomStack stack = new CustomStack(5);
+        DynamicStack stack = new DynamicStack(5);
+        
 
         stack.push(34);
         stack.push(4);
         stack.push(44);
         stack.push(24);
         stack.push(14);
+        stack.push(77);
 
         System.out.println(stack.pop());
         System.out.println(stack.pop());
