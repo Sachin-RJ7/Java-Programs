14277917c6d23411592c4daf1bcc133a475935d0
Sachin <sachinkrj077@gmail.com>
2022-09-06 07:22:03 +0500

    dynamicQueue

--- src/com/Sachin/Stack/QueueMain.java
+++ src/com/Sachin/Stack/QueueMain.java
@@ -13,17 +13,28 @@ public class QueueMain {
 //        System.out.println(queue.remove());
 //        queue.display();
 
-        CircularQueue queue = new CircularQueue(5);
-        queue.insert(3);
-        queue.insert(4);
-        queue.insert(5);
-        queue.insert(6);
-        queue.insert(7);
+//        CircularQueue queue = new CircularQueue(5);
+//        queue.insert(3);
+//        queue.insert(4);
+//        queue.insert(5);
+//        queue.insert(6);
+//        queue.insert(7);
+//
+//        queue.display();
+//        System.out.println(queue.remove());
+//        queue.remove();
+//        queue.insert(77);
+//        queue.display();
 
+        DynamicQueue queue = new DynamicQueue(5);
+        queue.insert(11);
+        queue.insert(22);
+        queue.insert(33);
+        queue.insert(44);
+        queue.insert(55);
+        queue.insert(66);
         queue.display();
-        System.out.println(queue.remove());
-        queue.remove();
-        queue.insert(77);
+        queue.insert(99);
         queue.display();
     }
 }
