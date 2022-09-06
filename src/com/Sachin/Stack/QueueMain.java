1504d5aa08a62e0f9aa465ff9e25acdf98f4873d
Sachin <sachinkrj077@gmail.com>
2022-09-06 06:57:11 +0500

    circularQueue

--- src/com/Sachin/Stack/QueueMain.java
+++ src/com/Sachin/Stack/QueueMain.java
@@ -2,7 +2,18 @@ package com.Sachin.Stack;
 
 public class QueueMain {
     public static void main(String[] args) throws Exception {
-        CustomQueue queue = new CustomQueue(5);
+//        CustomQueue queue = new CustomQueue(5);
+//        queue.insert(3);
+//        queue.insert(4);
+//        queue.insert(5);
+//        queue.insert(6);
+//        queue.insert(7);
+//
+//        queue.display();
+//        System.out.println(queue.remove());
+//        queue.display();
+
+        CircularQueue queue = new CircularQueue(5);
         queue.insert(3);
         queue.insert(4);
         queue.insert(5);
@@ -11,6 +22,8 @@ public class QueueMain {
 
         queue.display();
         System.out.println(queue.remove());
+        queue.remove();
+        queue.insert(77);
         queue.display();
     }
 }
