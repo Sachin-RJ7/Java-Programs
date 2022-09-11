dd809b60a00fd0245fbfdc444403343337285342
Sachin <sachinkrj077@gmail.com>
2022-09-11 07:52:28 +0500

    hcf

--- src/com/Sachin/Pseudocode/Main.java
+++ src/com/Sachin/Pseudocode/Main.java
@@ -2,16 +2,24 @@ package com.Sachin.Pseudocode;
 
 import java.util.Scanner;
 
+import static com.Sachin.Pseudocode.Pseudocode.gcd;
 import static com.Sachin.Pseudocode.Pseudocode.leapYear;
 
 public class Main {
     public static void main(String[] args) {
-        int year;
-        System.out.println("Please! Enter a year");
-        Scanner sc = new Scanner(System.in);
-        year = sc.nextInt();
-        leapYear(year);
+//        int year;
+//        System.out.println("Please! Enter a year");
+//        Scanner sc = new Scanner(System.in);
+//        year = sc.nextInt();
+//        leapYear(year);
 
+        int a, b;
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Ist number");
+        a = scn.nextInt();
+        System.out.println("Enter 2nd number");
+        b = scn.nextInt();
+        System.out.println("gcd = " + gcd(a, b));
     }
 
 
--- src/com/Sachin/Pseudocode/Pseudocode.java
+++ src/com/Sachin/Pseudocode/Pseudocode.java
@@ -1,7 +1,7 @@
 package com.Sachin.Pseudocode;
 
 public class Pseudocode {
-
+    // to find a leap year
     static public void leapYear (int year) {
         if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0 )) {
             System.out.println("It's a leap year");
@@ -10,5 +10,11 @@ public class Pseudocode {
         }
     }
 
+//    to find gcd or hcf of two numbers
+    static public int gcd(int a , int b) {
+        if(b == 0)
+            return a;
+        return gcd( b, a % b );
+    }
 
 }
