a19c7107a4adbb0873e6814412873d948246ef1a
Sachin <sachinkrj077@gmail.com>
2022-09-12 07:55:07 +0500

    find prime number

---
+++ src/com/Sachin/Pseudocode/Prime.java
@@ -0,0 +1,23 @@
+package com.Sachin.Pseudocode;
+
+import static com.Sachin.Math.Prime.isPrime;
+
+public class Prime {
+    static public boolean isPrime(int x) {
+        if (x <= 1)
+            return false;
+
+        int c = 2;
+        while ( c * c <= x) {
+            if (x % c == 0) {
+                return false;
+            }
+            c++;
+        }
+        return c * c > x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(71));
+    }
+}
