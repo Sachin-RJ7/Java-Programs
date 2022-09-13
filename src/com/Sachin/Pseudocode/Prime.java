8da903e09cd03d3731a33b5e8dceeb41613f612a
Sachin <sachinkrj077@gmail.com>
2022-09-13 06:57:11 +0500

    prime number in range

--- src/com/Sachin/Pseudocode/Prime.java
+++ src/com/Sachin/Pseudocode/Prime.java
@@ -1,5 +1,7 @@
 package com.Sachin.Pseudocode;
 
+import java.util.Scanner;
+
 public class Prime {
     static public boolean isPrime(int x) {
         if (x <= 1)
@@ -15,7 +17,41 @@ public class Prime {
         return c * c > x;
     }
 
-    public static void main(String[] args) {
-        System.out.println(isPrime(71));
+    private static boolean primeInRange(int n) {
+        // 1  and 0 is neither prime or composite
+        if (n == 0 || n==1)
+            return false;
+        
+        // 2 is a prime number
+        if (n == 2)
+            return true;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
+
+    public static void main(String[] args) {
+
+        System.out.println(isPrime(71));
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Lower bound of the interval");
+        int start = in.nextInt();
+        System.out.println("Enter Upper bound of the interval");
+        int last = in.nextInt();
+        System.out.printf("\nPrime number between %d and %d are: ", start, last);
+
+        for (int i = ++start; i < last ; i++) {
+            if (primeInRange(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        in.close();
+
+    }
+
 }
